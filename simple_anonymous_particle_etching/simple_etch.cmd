160ebaf75c257f424c44e238f73ff01ac13fbfd8
Utkarsh <ge84lel@mytum.de>
2026-05-22 14:31:26 +0530

    some changes

--- simple_anonymous_particle_etching/simple_etch.cmd
+++ simple_anonymous_particle_etching/simple_etch.cmd
@@ -8,9 +8,9 @@ line y location=1.0 spacing=0.1
 # 2. Initialize the grid space as a solid Silicon substrate
 init silicon
 
-# 3. Perform a simple geometric etch
-# This etches a 0.4 micron deep trench exactly in the middle (from X=0.3 to X=0.7)
-etch material=silicon type=anisotropic thickness=0.4 left=0.3 right=0.7
+# 3. Perform a clean geometric anisotropic etch
+# Removes silicon down to Y=0.4, bounded between X=0.3 and X=0.7
+etch material=silicon type=anisotropic thickness=0.4 coord={0.3 0.7}
 
 # 4. Save the resulting structure directly to a TDR file
 struct tdr=simple_etch_out.tdr
