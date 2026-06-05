2e9d1fec7c0baf236f595cef33d9cc0194faef7c
Deepali <deepali29795@gmail.com>
2026-06-05 16:32:36 +0530

    changed script

--- daily_automation_script.sh
+++ daily_automation_script.sh
@@ -10,12 +10,12 @@
 # Scenario 2
 echo "Running scenario 1..."
 cd cases
-# timeout 120s sprocess OxideMaskedHighAspectRatioEtch.cmd
+timeout 120s sprocess OxideMaskedHighAspectRatioEtch.cmd
 # timeout 120s sprocess SimpleRIESiliconTrench.cmd
 # timeout 120s sprocess BoschDRIE.cmd
 # timeout 120s sprocess DualLevelMEMSCavity.cmd
 # timeout 120s sprocess MultiStepTSVProcess.cmd   
-timeout 120s sprocess OxideMaskedHighAspectRatioEtch_withFlux.cmd
+# timeout 120s sprocess OxideMaskedHighAspectRatioEtch_withFlux.cmd
 cd ..
 
 # Git automation
